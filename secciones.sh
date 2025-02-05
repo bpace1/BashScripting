c858e3152ceed42b2e2420b5512aa38c87e6060c
Bruno Pace <brunopace2001@gmail.com>
2025-02-05 08:35:11 -0300

    Update secciones.sh

--- secciones.sh
+++ secciones.sh
@@ -16,10 +16,10 @@ REPOSITORIO="oh-my-git"
 # Se chequea si el directorio existe, si existe  imprime un mensaje. En caso contrario, clona el repositorio.
 if [ -d $REPOSITORIO ]
 then
-	echo "Existe el Repositorio de oh-my-git."
+        echo "Existe el Repositorio de oh-my-git."
 else
-	echo "Clonando Repositorio..."
-	$(git clone https://github.com/git-learning-game/oh-my-git.git)
+        echo "Clonando Repositorio..."
+        $(git clone https://github.com/git-learning-game/oh-my-git.git)
 fi
 
 
@@ -39,16 +39,24 @@ LEVELS_LIST=$(ls "$FULL_PATH")
 # Esta página es intiuitiva y arma la expresión https://regex-generator.olafneumann.org
 for LEVEL in $LEVELS_LIST
 do
-	if [$LEVEL = 'sequence' ]; then
-		continue
-	fi
-	for SUBLEVEL in $(ls $FULL_PATH/$LEVEL)
+        if [ "$LEVEL" = "sequence" ]; then
+                continue
+        fi
+        LEVEL_PATH=$FULL_PATH/$LEVEL
+        for SUBLEVEL in $(ls "$LEVEL_PATH")
 do
-	if [ $SUBLEVEL = 'sequence' ]; then
-		continue
-	fi
-	SUBLEVEL_PATH=$FULL_PATH/$LEVEL/$SUBLEVEL
-	echo nivel: $LEVEL - subnivel:  $SUBLEVEL
-	grep -oP  '\[\K[^][]*(?=])' $SUBLEVEL_PATH
+        if [ "$SUBLEVEL" = "sequence" ]; then
+                continue
+        fi
+        SUBLEVEL_PATH=$LEVEL_PATH/$SUBLEVEL
+        echo nivel: $LEVEL - subnivel:  $SUBLEVEL
+        #grep -oP  '\[\K[^][]*(?=])' "$SUBLEVEL_PATH"
+#       grep -E '^[a-zA-Z ]' "$SUBLEVEL_PATH"
+#
+        grep -E '^[a-zA-Z]+$' "$SUBLEVEL_PATH"
 done
 done
+
+# ¿Por qué los path están encerrado entre comillas dobles?
+# ¿Existe otra manera de resolver este ejercicio?
+# ¿Es conveniente implementar funciones? ¿Dónde?
