44e7efa0156b413000a78d116d1d635aa047d08b
Bruno Pace <brunopace2001@gmail.com>
2025-02-05 21:24:48 -0300

    Update secciones.sh

--- secciones.sh
+++ secciones.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# En este script vamos a chequear que secciones tiene el juego "oh-my-git".
+# En este script vamos a chequear que secciones de los diferentes subniveles de cada nivel que tiene el juego "oh-my-git".
+# ¿Cuáles son las secciones? Las referidas a la configuración del juego, como por ejemplo: [congrats] y [win]
 
 # Pero antes de eso, tenemos que clonar el repositorio. Para estar seguros, lo chequeo.
 
-# Extraigo el path para evitar errores en la ejecución.
-MY_PATH=$(pwd)
-
 echo "Inicio de chequeo de repositorio en el directorio."
 
-#echo $MY_PATH
 
 # Guardo en una variable el nombre de la carpeta del juego oh-my-git.
 REPOSITORIO="oh-my-git"
@@ -27,6 +24,10 @@ fi
 
 # --- Me aseguré que el repositorio exista en el directorio donde estoy trabajando. Ahora empiezo el chequeo de secciones. ---
 
+# Extraigo el path para evitar errores en la ejecución.
+MY_PATH=$(pwd)
+
+#echo $MY_PATH
 
 LEVELS="levels"
 
@@ -38,24 +39,23 @@ LEVELS_LIST=$(ls "$FULL_PATH")
 
 echo "Filtrando secciones de los niveles."
 
-# Para filtrar las secciones precisamos utilizar REGEX.
-# Esta página es intiuitiva y arma la expresión https://regex-generator.olafneumann.org
-for LEVEL in $LEVELS_LIST
+# Para filtrar las secciones precisamos utilizar REGEX (expresiones regulares).
+# Recomendación: esta página es intiuitiva y arma la expresión necesaria https://regex-generator.olafneumann.org
+
+for LEVEL in $LEVELS_LIST  # Itero sobre los diferentes niveles. Es decir, paso por cada uno de ellos.
 do
         if [ "$LEVEL" = "sequence" ]; then
                 continue
         fi
         LEVEL_PATH=$FULL_PATH/$LEVEL
-        for SUBLEVEL in $(ls "$LEVEL_PATH")
+        for SUBLEVEL in $(ls "$LEVEL_PATH") # Itero ahora sobre los sub-niveles. Esto se llama for anidado. Tengo un for dentro de otro.
+						# El resultado será "pasar" por cada nivel con sus respectivo(s) sub-nivel(es).
 do
         if [ "$SUBLEVEL" = "sequence" ]; then
                 continue
         fi
         SUBLEVEL_PATH=$LEVEL_PATH/$SUBLEVEL
         echo nivel: $LEVEL - subnivel:  $SUBLEVEL >> secciones.txt # ¿Por qué se utilizan dos signos >>?
-        #grep -oP  '\[\K[^][]*(?=])' "$SUBLEVEL_PATH"
-#       grep -E '^[a-zA-Z ]' "$SUBLEVEL_PATH"
-#
         grep -oP '^\s*\[\K[^]]*(?=\]\s*$)' "$SUBLEVEL_PATH" >> secciones.txt
 done
 done
@@ -64,4 +64,4 @@ echo "Se guardó un archivo 'secciones.txt' con la información."
 
 # ¿Por qué los path están encerrado entre comillas dobles?
 # ¿Existe otra manera de resolver este ejercicio?
-# ¿Es conveniente implementar funciones? ¿Dónde?
+# ¿Es conveniente implementar funciones? Si la rta. es sí, ¿Dónde?
